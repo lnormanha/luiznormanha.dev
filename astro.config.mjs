033c9d34fcf2aa8b5cf430c1bdd5fcf4ef44c2eb
lnormanha <luiznormanha@gmail.com>
2023-09-07 21:05:22 -0300

    feat: avoid full page re-mount

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { i18n, defaultLocaleSitemapFilter } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
@@ -6,33 +6,39 @@ import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
 const defaultLocale = "en";
 const locales = {
-  "en": "en-US",
+  en: "en-US",
   // the `defaultLocale` value must present in `locales` keys
-  "pt": "pt-BR"
+  pt: "pt-BR",
 };
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://luiznormanha.dev",
   trailingSlash: "always",
   build: {
-    format: "directory"
+    format: "directory",
   },
   output: "hybrid",
-  integrations: [i18n({
-    locales,
-    defaultLocale,
-  }), sitemap({
-    i18n: {
+  integrations: [
+    i18n({
       locales,
-      defaultLocale
-    },
-    filter: defaultLocaleSitemapFilter({
-      defaultLocale
+      defaultLocale,
     }),
-  }), react()],
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: defaultLocaleSitemapFilter({
+        defaultLocale,
+      }),
+    }),
+    react(),
+  ],
   adapter: vercel({
-    includeFiles: ['./src/pages/api/send-email.json.ts'],
+    includeFiles: ["./src/pages/api/send-email.json.ts"],
   }),
+  experimental: {
+    viewTransitions: true,
+  },
 });
