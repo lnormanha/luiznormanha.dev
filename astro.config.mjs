4eb6cb3ebe674264ee3a6b5b5b3040ad0dff0a8e
Luiz <luiznormanha@gmail.com>
2024-01-13 17:59:52 -0300

    feat: clean project for revamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
-import { i18n, defaultLocaleSitemapFilter } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
-const defaultLocale = "en";
-const locales = {
-  en: "en-US",
-  // the `defaultLocale` value must present in `locales` keys
-  pt: "pt-BR",
-};
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,26 +12,8 @@ export default defineConfig({
     format: "directory",
   },
   output: "hybrid",
-  integrations: [
-    i18n({
-      locales,
-      defaultLocale,
-    }),
-    sitemap({
-      i18n: {
-        locales,
-        defaultLocale,
-      },
-      filter: defaultLocaleSitemapFilter({
-        defaultLocale,
-      }),
-    }),
-    react(),
-  ],
+  integrations: [react()],
   adapter: vercel({
     includeFiles: ["./src/pages/api/send-email.json.ts"],
   }),
-  experimental: {
-    viewTransitions: true,
-  },
 });
